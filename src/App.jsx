bd280c28efdadfdec9a3c372e9f70e44ac1dbf6a
juanmacasal <juanmacasal2002@gmail.com>
2024-01-23 16:04:18 -0300

    commit dos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './componentes/NavBar/NavBar'
 import ItemListContainer from './componentes/ItemListContainer/ItemListContainer'
+import ItemCount from './componentes/ItemCount/ItemCount';
 
 const App = () => {
   return (
     <>
     <Navbar/>
     <ItemListContainer greeting={"Zapatillas" }  />
+    <ItemCount initial={1} stock={10} onAdd={(quantity) => console.log('Cantidad agregada ',quantity)}/>
     </>
   )
 }
