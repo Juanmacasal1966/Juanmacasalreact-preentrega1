25ed511496a725914aa22126dfaa6e3e5a263e3e
juanmacasal <juanmacasal2002@gmail.com>
2024-01-31 15:10:04 -0300

    correcion quinto commit

--- src/componentes/ItemDetail/ItemDetail.jsx
+++ src/componentes/ItemDetail/ItemDetail.jsx
@@ -1,17 +1,29 @@
 
 import React from 'react';
+import { useParams } from 'react-router-dom';
 import './ItemDetail.css';
 
-const ItemDetail = ({ id, nombre, precio, img }) => {
+const ItemDetail = () => {
+  
+  const { id } = useParams();
+
+  const itemDetails = {
+    id: id,
+    nombre: 'Nombre del ítem',
+    precio: 'Precio del ítem',
+    img: 'URL de la imagen del ítem',
+  };
+
   return (
     <div className="item-detail-container">
-      <h2 className="name">Nombre: {nombre} </h2>
-      <h3 className="price">Precio: {precio} </h3>
-      <p className="id">ID: {id} </p>
+      <h2 className="name">Nombre: {itemDetails.nombre} </h2>
+      <h3 className="price">Precio: {itemDetails.precio} </h3>
+      <p className="id">ID: {itemDetails.id} </p>
       <p></p>
-      <img src={img} alt={nombre} className="item-image" />
+      <img src={itemDetails.img} alt={itemDetails.nombre} className="item-image" />
     </div>
   );
 }
 
 export default ItemDetail;
+
